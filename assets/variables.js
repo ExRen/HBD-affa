8b7b29565479ef9462c60a3c55d5a805f75c2e8f
ExRen <65204321+ExRen@users.noreply.github.com>
2020-05-22 20:40:51 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,10 +1,10 @@
 export default {
-  nicknames: ["Ava", "Babe", "Sayang", "Avakuu", "Cintuu"],
+  nicknames: ["Affa", "Beb", "Sayang"],
   greetings: {
-    evening: "Good Evening",
-    afternoon: "Good Afternoon",
-    day: "Good Day",
-    morning: "Good Morning",
-    night: "Good Night"
+    evening: "Tidur woe",
+    afternoon: "Sore",
+    day: "Dah siang",
+    morning: "Pagi",
+    night: "Malam"
   }
 };
