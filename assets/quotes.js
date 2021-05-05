f9dc0a3764b6c419505a8a7437852c8ac88ebe83
ExRen <65204321+ExRen@users.noreply.github.com>
2021-05-05 12:20:33 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,9 +1,9 @@
 // prettier-ignore
 export default [
-    { author: "Bima_a", quotes: "HBD affa" }
-    { author: "Bima_a", quotes: "love you <3" }
-    { author: "Bima_a", quotes: "sorry" }
-    { author: "Bima_a", quotes: "sorry can't be the best" }
-    { author: "Bima_a", quotes: "I hope what you dream is achieved soon" }
+    { author: "Bima_a", quotes: "HBD affa" },
+    { author: "Bima_a", quotes: "love you <3" },
+    { author: "Bima_a", quotes: "sorry" },
+    { author: "Bima_a", quotes: "sorry can't be the best" },
+    { author: "Bima_a", quotes: "I hope what you dream is achieved soon" },
 
 ];
